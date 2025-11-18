920451461c19de9f51c6c0bbbfdaf5611e0573ac
matat <matatapol22@gmail.com>
2025-11-18 18:05:58 +0700

    Логотип и ссылки

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,10 +3,25 @@ import '../styles/style.css';
 
 const Header = () => {
     return (
-        <div className="container">
-            <header className="app-header">
-            </header>
-        </div>
+        <header className="app-header">
+            <div className="container">
+                <div className='header-content'>
+                    <div className='header-logo'>
+                        <img src="../image/logo.jpg" alt="Logo" />
+                        <a className='header-logo-name' href="#">Житница</a>
+                    </div>
+                    <div className='header-categories'>
+                        <a href="#">Главная</a>
+                        <a href="#">Каталог</a>
+                        <a href="#">О нас</a>
+                        <a href="#">Заказы</a>
+                        <a href="#">Доставка</a>
+                        <a href="#">Оплата</a>
+                    </div>
+
+                </div>
+            </div>
+        </header>
     )
 
 };

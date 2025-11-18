10e842c92c57d58b192cfcced91f15fcb6d74745
matat <matatapol22@gmail.com>
2025-11-18 23:29:41 +0700

    Header is done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../styles/style.css';
+import logo from '../image/logo.svg';
+import search from '../image/search.svg';
+import cart from '../image/cart.svg';
+import user from '../image/user.svg';
 
 const Header = () => {
     return (
@@ -7,8 +11,10 @@ const Header = () => {
             <div className="container">
                 <div className='header-content'>
                     <div className='header-logo'>
-                        <img src="../image/logo.jpg" alt="Logo" />
-                        <a className='header-logo-name' href="#">Житница</a>
+                        <a href='#'>
+                            <img src={logo} alt="Logo" />
+                            <p>Житница</p>  
+                        </a>
                     </div>
                     <div className='header-categories'>
                         <a href="#">Главная</a>
@@ -17,6 +23,11 @@ const Header = () => {
                         <a href="#">Заказы</a>
                         <a href="#">Доставка</a>
                         <a href="#">Оплата</a>
+                    </div>
+                    <div className='header-icons'>
+                        <a href="#"><img src={search} alt="Search" /></a>
+                        <a href="#"><img src={cart} alt="Cart" /></a>
+                        <a href="#"><img src={user} alt="User" /></a>
                     </div>
 
                 </div>
